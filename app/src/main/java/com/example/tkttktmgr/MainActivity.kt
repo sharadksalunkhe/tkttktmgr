4bde7fd23872631ef3dc76fdb27a344d58257bb5
sharadksalunkhe <sharadksalunkhe@gmail.com>
2022-04-24 12:35:16 +0530

    Initial commit

---
+++ app/src/main/java/com/example/tkttktmgr/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.tkttktmgr
+
+import android.content.Intent
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.LiveData
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.tkttktmgr.databinding.ActivityMainBinding
+import com.example.tkttktmgr.login.data.model.LoggedInUser
+import com.example.tkttktmgr.login.ui.login.LoginActivity
+import com.example.tkttktmgr.login.ui.login.LoginResult
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+}
